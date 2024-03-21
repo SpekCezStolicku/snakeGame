83da09c5b36698d6fd02fff6c9daa98189a9f2c3
Speky <martin.mozucha@gmail.com>
2024-03-21 17:32:34 +0100

    global css presets

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,11 @@
   <main></main>
 </template>
 
-<style scoped></style>
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'VT323', 'Courier New', Courier, monospace;
+}
+</style>
