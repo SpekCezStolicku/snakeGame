7ea438302a5995c7a281f36b1fec51cb20068a8b
Speky <martin.mozucha@gmail.com>
2024-03-21 17:07:09 +0100

    create game settings

---
+++ src/store/gameSettings.ts
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia'
+
+export const useGameStore = defineStore('game', {
+  state: () => ({
+    gameSpeed: 5,
+    score: 0,
+    playground: {
+      width: 600,
+      height: 400
+    }
+  }),
+  actions: {
+    increaseScore() {
+      this.score++
+    },
+    setGameSpeed(speed: number) {
+      this.gameSpeed = speed
+    }
+  }
+})
