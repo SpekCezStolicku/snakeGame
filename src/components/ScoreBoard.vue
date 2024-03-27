1970c70880d742e9c5b3a1c2bc3108bbed9cf118
Speky <martin.mozucha@gmail.com>
2024-03-28 00:59:47 +0100

    add score board component

---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex items-center justify-between scoreboard">
+    <h1>
+      Score: <b>{{ gameStore.score }}</b>
+    </h1>
+    <div class="flex items-center justify-center hightscore">
+      <img src="/src/assets/images/crown.png" alt="crown" width="25" />
+      <h1>
+        Highscore: <b>{{ gameStore.score }}</b>
+      </h1>
+      <button>Leaderboard</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useGameStore } from '@/store/gameSettings'
+
+// store
+const gameStore = useGameStore()
+</script>
