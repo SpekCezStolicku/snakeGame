fc4427c840b687568131c5a38e3f385b40bd3235
Speky <martin.mozucha@gmail.com>
2024-03-28 19:01:49 +0100

    display level

--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -3,10 +3,11 @@
     <h1>
       Score: <b>{{ gameStore.score }}</b>
     </h1>
+    <h1 class="font-bold">Level {{ gameStore.level }}</h1>
     <div class="flex items-center justify-center hightscore">
       <img src="/src/assets/images/crown.png" alt="crown" width="25" />
       <h1>
-        Your highscore: <b>{{ gameStore.score }}</b>
+        Your Highscore: <b>{{ gameStore.score }}</b>
       </h1>
       <button class="hidden px-4 text-xl font-bold">Leaderboard</button>
     </div>
