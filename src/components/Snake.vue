d2830f0177984645adc2f6730826d39b1fd49510
Speky <martin.mozucha@gmail.com>
2024-03-29 00:57:41 +0100

    turning the head depending on the direction

--- src/components/Snake.vue
+++ src/components/Snake.vue
@@ -3,7 +3,7 @@
   <div
     v-for="(segment, index) in snakePosition"
     :key="`segment-${index}`"
-    :style="{ gridColumnStart: segment.x, gridRowStart: segment.y }"
+    :style="getSegmentStyle(segment, index)"
   >
     <img
       :src="`/src/assets/images/${getSegmentImage(index)}.png`"
@@ -16,6 +16,7 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useGameStore } from '@/store/gameSettings'
+import type { Position } from '@/types/types'
 
 const gameStore = useGameStore()
 
@@ -34,6 +35,32 @@ function getSegmentImage(index: number) {
     return body
   }
 }
+
+function getSegmentStyle(segment: Position, index: number) {
+  let rotation = 0
+  if (index === 0) {
+    // Head rotation
+    switch (gameStore.direction) {
+      case 'UP':
+        rotation = 0
+        break
+      case 'RIGHT':
+        rotation = 90
+        break
+      case 'DOWN':
+        rotation = 180
+        break
+      case 'LEFT':
+        rotation = -90
+        break
+    }
+  }
+  return {
+    gridColumnStart: segment.x,
+    gridRowStart: segment.y,
+    transform: `rotate(${rotation}deg)`
+  }
+}
 </script>
 
 <style scoped>
@@ -42,5 +69,6 @@ function getSegmentImage(index: number) {
   width: 35px;
   transform: translate(-23%, -50%);
   z-index: 10;
+  max-width: none;
 }
 </style>
