4f1b8b9a8e9a23116b7f8352182943149390b4f1
Speky <martin.mozucha@gmail.com>
2024-04-16 23:11:59 +0200

    update firestore functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,22 @@
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+admin.initializeApp()
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const trimScores = functions.firestore
+  .document('scores/{scoreId}')
+  .onCreate(async (snap, context) => {
+    const scoresRef = admin.firestore().collection('scores')
+
+    const scoresSnapshot = await scoresRef.orderBy('score').get()
+
+    if (scoresSnapshot.size > 20) {
+      const toDeleteCount = scoresSnapshot.size - 20
+      const batch = admin.firestore().batch()
+
+      scoresSnapshot.docs.slice(0, toDeleteCount).forEach((doc) => {
+        batch.delete(doc.ref)
+      })
+
+      await batch.commit()
+    }
+  })
