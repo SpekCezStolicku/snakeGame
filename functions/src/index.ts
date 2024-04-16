a9aaf79a54770a23208b3cb9a016f621523a5b25
Speky <martin.mozucha@gmail.com>
2024-04-16 17:45:24 +0200

    add firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,7 @@
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -13,6 +13,6 @@ const firebaseConfig = {
 }
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
 export const analytics = getAnalytics(app)
