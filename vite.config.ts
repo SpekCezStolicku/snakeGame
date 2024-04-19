f1ef4712f25a28893266ae7a1258d5cae924e1f4
Speky <martin.mozucha@gmail.com>
2024-04-19 22:00:35 +0200

    vite setup for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
+const repoName = 'snakeGame'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/'
 })
